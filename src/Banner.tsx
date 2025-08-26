11ad5901bd30d1ddd657dc7dd6d6dd41723104e9
Zachary Williams <zacharywilliams080802@gmail.com>
2025-08-26 14:01:39 -0400

    fixed banner in mobile

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -2,13 +2,21 @@ function Banner({ loopTime }: { loopTime: number }) {
 
   return (
     <>
-      <div className="w-full h-20 text-3xl flex flex-row items-center overflow-hidden bg-black relative ">
-        <div className="text-white overflow-hidden inline absolute whitespace-nowrap" style={{ animation: `scroll-left ${loopTime}s linear infinite` }}>
+      <div className="w-full h-20 text-sm md:text-3xl flex flex-row items-center overflow-hidden bg-black relative">
+
+        <div className="text-white overflow-hidden inline absolute whitespace-nowrap"
+          style={{ animation: `banner ${loopTime}s linear infinite` }}>
           WE ARE CLOSED FOR THE 2024 SEASON
         </div>
-        <div className="text-white overflow-hidden inline absolute whitespace-nowrap" style={{ animation: `scroll-left ${loopTime}s linear infinite ${loopTime / 2}s`, transform: "translateX(100vw)" }}>
+
+        <div className="text-white overflow-hidden inline absolute whitespace-nowrap"
+          style={{
+            transform: "translateX(100vw)",
+            animation: `banner ${loopTime}s linear infinite ${loopTime / 2}s`
+          }}>
           WE ARE CLOSED FOR THE 2024 SEASON
         </div>
+
       </div>
     </>
   )
